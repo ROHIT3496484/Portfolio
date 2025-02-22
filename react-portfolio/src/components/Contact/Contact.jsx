7d04b72d64bd37b78907ec0a89de6952c39eb1ed
ROHIT3496484 <rohitrana349@gmail.com>
2025-02-22 13:18:45 +0530

    changes

--- react-portfolio/src/components/Contact/Contact.jsx
+++ react-portfolio/src/components/Contact/Contact.jsx
@@ -13,7 +13,7 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:rohitrana349@gmail.com">myemail@email.com</a>
+          <a href="mailto:rohitrana349@gmail.com">rohitrana349@gmail.com</a>
         </li>
         <li className={styles.link}>
           <img
